e02efb96015818ec0974c642573dc1f8158186ae
Spazcool <douglas.wrightiii@gmail.com>
2021-02-03 13:18:22 -0500

    add sumtotal func to list

--- src/components/List.js
+++ src/components/List.js
@@ -23,10 +23,18 @@ function List(props){
     )
   }
 
-  // clickevent to toggle pending value when row is clicked on
-  // make category, quantity & price editble text fields
+  const sumPending = (arr) => {
+    const prices = arr.map((el) => el.price);
+    const sum = prices.reduce((a, b) => a + b);
+    return sum;
+  }
 
-  return(    
+  // make category, quantity & price editble text fields
+  const pending = props.data.filter((el) => el.pending === true).map((el)=> createRow(el)); //{/* NEED TO APPLY THE SORT ON THE EL.NAME BEFORE CALLING THE MAP */}
+  const crossedOff = props.data.filter((el) => el.pending === false).map((el)=> createRow(el));
+  const pendingTotal = sumPending(props.data.filter((el) => el.pending === true));
+  const halfTableWidth = Object.keys(props.data[0]).length / 2;
+  return(
     <>
       <h4>Pending</h4>
       <Tables striped bordered hover>
@@ -34,8 +42,12 @@ function List(props){
           {createRow(Object.keys(props.data[0]), null, true)}
         </thead>
         <tbody>
-          {props.data.filter((el) => el.pending === true).map((el)=> createRow(el))}
-          <tr>summ of prices</tr>
+          {pending}
+          <tr>
+            <td colSpan={halfTableWidth}>Pending Total: </td>
+            <td colSpan={halfTableWidth}>{pendingTotal}</td>
+          </tr>
+
         </tbody>
       </Tables>
       <h4>Crossed Off</h4>
@@ -44,7 +56,7 @@ function List(props){
           {createRow(Object.keys(props.data[0]))}
         </thead>
         <tbody>
-          {props.data.filter((el) => el.pending === false).map((el)=> createRow(el))}
+          {crossedOff}
         </tbody>
       </Tables>
     </>
