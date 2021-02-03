a4a1db3a9cf79ecb1ecd45757b8ee4a5c6e563cc
Spazcool <douglas.wrightiii@gmail.com>
2021-02-02 22:31:06 -0500

    split table into crossed off and pending sections

--- src/components/List.js
+++ src/components/List.js
@@ -5,11 +5,11 @@ import '../App.css';
 
 function List(props){
 
-  const createRow = (datum, index, term) => {
+  const createRow = (datum, index) => {
     const att = Object.keys(datum);
     const cells = att.map((cell) => (
-      <td key={`${cell}-cell-${index}`}>{datum[cell] === undefined ? 'X' : datum[cell]}</td>
-    ));
+        <td key={`${cell}-cell-${index}`}>{datum[cell] === undefined ? 'X' : datum[cell]}</td>
+      ));
 
     return (  
       <tr>
@@ -18,15 +18,27 @@ function List(props){
     )
   }
 
-  return(        
-    <Table striped bordered hover>
-      <thead>
-        {createRow(Object.keys(props.data[0]))}
-      </thead>
-      <tbody>
-        {props.data.map((el)=> createRow(el))}
-      </tbody>
-    </Table>
+  return(    
+    <>
+      <h4>Pending</h4>
+      <Table striped bordered hover>
+        <thead>
+          {createRow(Object.keys(props.data[0]))}
+        </thead>
+        <tbody>
+          {props.data.filter((el) => el.pending === true).map((el)=> createRow(el))}
+        </tbody>
+      </Table>
+      <h4>Crossed Off</h4>
+      <Table striped bordered hover>
+        <thead>
+          {createRow(Object.keys(props.data[0]))}
+        </thead>
+        <tbody>
+        {props.data.filter((el) => el.pending === false).map((el)=> createRow(el))}
+        </tbody>
+      </Table>
+    </>
   )
 }
 
