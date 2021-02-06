85019b80b330739e3fd0972008196fbfb2c95131
Spazcool <douglas.wrightiii@gmail.com>
2021-02-05 23:21:20 -0500

    remove dead code and add alnk to personal site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,11 @@
 import React from 'react';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
 import '../App.css';
 
 function Footer(){
   return(        
     <footer className="footer">
       <div className="container">
-        <span className="text-muted">
-          some footer text goes here
-        </span>
+        <span className="text-muted">Built by: <a href="http://www.spazcool.com">Douglas Wright</a></span>
       </div>
     </footer>
   )
